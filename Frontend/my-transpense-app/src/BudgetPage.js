1b999cff12f73274bcf56905a65280a5fdab6194
admin <sayalisn4@gmail.com>
2024-02-25 14:04:15 -0800

    Changes to budget page

--- Frontend/my-transpense-app/src/BudgetPage.js
+++ Frontend/my-transpense-app/src/BudgetPage.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
 
-const HomePage = () => {
+const BudgetPage = () => {
   return (
     <div>
-    <div className='total-budget'>
+        <div className='total-budget'>
 
-    </div>
-    <div className='visualizations'>
-        
-    </div>
-    <div className='budget-alloc'>
-        
-    </div>
-    <div className='transactions'>
-        
-    </div>
+        </div>
+        <div className='visualizations'>
+            
+        </div>
+        <div className='data-accordions'>
+
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+            >
+            Budget Allocation
+            </AccordionSummary>
+            <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+            >
+            Transactions
+            </AccordionSummary>
+            <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </AccordionDetails>
+        </Accordion>
+        </div>
     </div>
   );
 };
 
-export default HomePage;
+export default BudgetPage;
