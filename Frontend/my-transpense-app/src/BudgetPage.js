6e74947be45a0ad7ac7fd3d2482cd7c76e982a89
admin <sayalisn4@gmail.com>
2024-02-25 13:57:27 -0800

    Create budget page

---
+++ Frontend/my-transpense-app/src/BudgetPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+    <div className='total-budget'>
+
+    </div>
+    <div className='visualizations'>
+        
+    </div>
+    <div className='budget-alloc'>
+        
+    </div>
+    <div className='transactions'>
+        
+    </div>
+    </div>
+  );
+};
+
+export default HomePage;
